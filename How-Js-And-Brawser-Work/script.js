6ca9eec800660f5354c63a383e3e2dafe8db7947
MdTasib <ohidulalam442@gmail.com>
2022-03-03 10:24:22 +0600

    setTimeout function

---
+++ How-Js-And-Brawser-Work/script.js
@@ -0,0 +1,32 @@
+// //////////
+// setTimeout simple Asynchronous JS using
+function hello() {
+	console.log("Hello world");
+}
+
+/* console.log("I am number 1");
+console.log("I am number 2");
+hello();
+console.log("I am number 3"); */
+
+/* console.log("I am number 1");
+console.log("I am number 2");
+setTimeout(hello);
+console.log("I am number 3"); */
+
+/* console.log("I am number 1");
+console.log("I am number 2");
+setTimeout(function () {
+	console.log("setTime out function runing");
+});
+console.log("I am number 3"); */
+
+/* console.log("I am number 1");
+console.log("I am number 2");
+// setTimeout(function () {
+// 	console.log("setTime out function runing");
+// });
+setTimeout(hello, 4000);
+setTimeout(() => console.log("arrow function running"), 3000);
+console.log("I am number 3");
+ */
