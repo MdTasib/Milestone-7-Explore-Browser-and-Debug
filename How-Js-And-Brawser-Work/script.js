7b01b40f7b039cf04c3d3661be1100340e0fc8b3
MdTasib <ohidulalam442@gmail.com>
2022-03-03 10:45:41 +0600

    async

--- How-Js-And-Brawser-Work/script.js
+++ How-Js-And-Brawser-Work/script.js
@@ -30,3 +30,27 @@ setTimeout(hello, 4000);
 setTimeout(() => console.log("arrow function running"), 3000);
 console.log("I am number 3");
  */
+
+// ////////////////////////////
+// Recognize fetch as an Asynchronous activity
+console.log(1);
+console.log(2);
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then(response => response.json())
+	.then(json => console.log(json));
+
+// async function dataLoad() {
+// 	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+// 	const data = await response.json();
+// 	console.log(data);
+// }
+
+// dataLoad();
+
+console.log(3);
+console.log(4);
+
+for (let i = 0; i < 1000; i++) {
+	console.log(i);
+}
